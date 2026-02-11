0eda7e1cdb68fd7f6d0ea797d5a6378e6293326d
Abhisheksharmaatri <a.ep.sharma@accenture.com>
2026-02-11 15:13:05 +0530

    Fixing order fetch issue

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -7,7 +7,7 @@
 const SERVICE_URLS = {
   product: "https://market-place-product-o37u.onrender.com/api",
   inventory: "https://market-place-inventory.onrender.com/api",
-  order: "https://market-place-user.onrender.com/api",
+  order: "https://market-place-order.onrender.com/api",
 };
 
 export default SERVICE_URLS;
