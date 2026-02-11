1764acf360e34fc88c8fe9e945ef0b8e9c2c5b1e
Abhisheksharmaatri <a.ep.sharma@accenture.com>
2026-02-11 11:42:51 +0530

    Solving the login issue

--- user-service/src/main/java/org/abhishek/service/AuthService.java
+++ user-service/src/main/java/org/abhishek/service/AuthService.java
@@ -1,6 +1,7 @@
 package org.abhishek.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.abhishek.config.JwtUtil;
 import org.abhishek.dto.LoginRequest;
 import org.abhishek.model.User;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthService {
 
     private final UserRepository userRepository;
@@ -17,13 +19,17 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     public String login(LoginRequest request) {
+        log.info("Started logging with email id :{}",request.getMail());
 
         User user = userRepository.findByMail(request.getMail().toLowerCase())
                 .orElseThrow(() -> new RuntimeException("Invalid credentials"));
 
+        log.info("Found the user for login for email id: {}", request.getMail());
+
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new RuntimeException("Invalid credentials");
         }
+        log.info("Generated the login token for email id: {}", request.getMail());
 
         return jwtUtil.generateToken(user);
     }
